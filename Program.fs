a75b82c66023adfe7f48cf28789ddc6d779f5654
Bernardo Amorim <contato@bamorim.com>
2020-12-02 23:41:15 -0300

    Day2

--- Program.fs
+++ Program.fs
@@ -36,6 +36,47 @@ module Day1 =
 
     let day = { num = 1; parseFile = parseFile; part1 = part1; part2 = part2 }
 
+module Day2 =
+    open System.Text.RegularExpressions
+
+    type Entry = int * int * char * string
+
+    let lineRegex = "(\d+)-(\d+) (\w): (\w+)"
+
+    let parseLine (line: string): Entry =
+        let matched = Regex.Match(line, lineRegex)
+        let groups = matched.Groups
+        ((int groups.[1].Value), (int groups.[2].Value), groups.[3].Value.Chars(0), groups.[4].Value)
+
+    let parseFile (filename: string): seq<Entry> =
+        filename
+        |> System.IO.File.ReadAllLines
+        |> Seq.map parseLine
+
+    let part1 (entries : seq<Entry>) : int =
+        let validEntry ((min, max, chr, str): Entry): Boolean =
+            let occurrences =
+                str.ToCharArray()
+                |> Seq.ofArray
+                |> Seq.filter (fun x -> x = chr)
+                |> Seq.length
+            occurrences >= min && occurrences <= max
+        entries
+        |> Seq.filter validEntry
+        |> Seq.length
+
+    let part2 (entries : seq<Entry>) : int =
+        let charCheck (str: String) chr idx =
+            str.Chars(idx - 1) = chr
+        let validEntry ((idx1, idx2, chr, str): Entry): Boolean =
+            (charCheck str chr idx1) <> (charCheck str chr idx2)
+
+        entries
+        |> Seq.filter validEntry
+        |> Seq.length
+
+    let day = { num = 2; parseFile = parseFile; part1 = part1; part2 = part2 }
+
 let runDay (day: Day<_, _, _>) =
   let filename = System.IO.Path.Combine(__SOURCE_DIRECTORY__, (sprintf "./inputs/%d.txt" day.num))
   let parsed = day.parseFile filename
@@ -45,6 +86,7 @@ let runDay (day: Day<_, _, _>) =
 
 [<EntryPoint>]
 let main argv =
-    runDay Day1.day
+    // For now I'll just keep changing the day to run
+    runDay Day2.day
 
     0 // return an integer exit code
